0b1f68b323408473424d45e59209ef69ef2fe633
Nicolás Hernández <IngNicolas8@gmail.com>
2018-08-19 20:58:41 -0300

    principio de validacion javascript

---
+++ Vivero/Vivero/Scripts/MisScripts.js
@@ -0,0 +1,18 @@
+﻿//Modal con mensajes para validacion
+
+function validar_documento(mensaje, dni) {
+    var controlDNI = document.getElementById(dni);
+    if (controlDNI.value.length < 7) {
+        $('#validaciones').modal();
+    }
+}
+
+//Fin de modal
+
+//Funcion de prueba
+
+function FuncionDePrueba() {
+    alert("andando")
+}
+
+//
